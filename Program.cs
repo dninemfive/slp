f802a584bf58bbc7f22848ec5e948f1937f38def
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-03-13 16:15:00 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal static class Program
+{
+    internal static TimeOnly StartTime = new(0, 30), EndTime = new(6, 0);
+    internal static TimeOnly TimeNow => TimeOnly.FromDateTime(DateTime.Now);
+    private static void Main(string[] args)
+    {
+        if (TimeNow > EndTime || TimeNow < StartTime)
+            SleepUntil(StartTime);
+    }
+    internal static int Minutes(this int minutes)
+        => minutes * 60 * 1000;
+    private static void SleepUntil(TimeOnly time)
+    {
+        int delay = (int)(time -  TimeNow).TotalMilliseconds;
+        Thread.Sleep(delay);
+    }
+}
