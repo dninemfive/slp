566ae3919e58fddc565ab417bc776a843711ddce
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-03-14 02:11:29 -0700

    revert start and end times to do string parsing

--- DieConfig.cs
+++ DieConfig.cs
@@ -3,18 +3,18 @@ using System.Text.Json.Serialization;
 
 namespace die;
 [method: JsonConstructor]
-public class DieConfig(TimeOnly startTime, TimeOnly endTime, double minutesBetweenCloseAttempts, List<ProcessTargeter> close, List<ProcessTargeter> allow)
+public class DieConfig(string startTime, string endTime, double minutesBetweenCloseAttempts, List<ProcessTargeter> close, List<ProcessTargeter> allow)
 {
-    public static readonly DieConfig Default = new(new(0, 30),
-                                                   new(10),
+    public static readonly DieConfig Default = new("12:30 AM",
+                                                   "10:00 AM",
                                                    10,
                                                    [new(ProcessTargetType.ProcessLocation, @"C:\Program Files (x86)\Steam"),
                                                     new(ProcessTargetType.MainWindowTitle, "Minecraft")],
                                                    [new(ProcessTargetType.ProcessName, "CrashHandler")]);
     [JsonInclude]
-    public TimeOnly StartTime { get; private set; } = startTime;
+    public TimeOnly StartTime { get; private set; } = TimeOnly.Parse(startTime);
     [JsonInclude]
-    public TimeOnly EndTime { get; private set; } = endTime;
+    public TimeOnly EndTime { get; private set; } = TimeOnly.Parse(endTime);
     [JsonInclude]
     public double MinutesBetweenCloseAttempts { get; private set; } = minutesBetweenCloseAttempts;
     [JsonInclude]
